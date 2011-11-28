b31be024d691702ecfb16d402de975c21c0eab4b
David Schneider <david.schneider@picle.org>
2011-11-28 21:24:39 +0100

    refactor entry point and store data as json

--- latitude/__main__.py
+++ latitude/__main__.py
@@ -1,6 +1,12 @@
-from client import Latitude
-import sys
 import gflags
+import sys
+from datetime import date, timedelta
+
+from latitude.client import Latitude
+from latitude.data import JSON
+from latitude.exporter.filesystem import FileSystem
+
+gflags.DEFINE_boolean('debug', False, 'produces debugging output')
 
 def main(argv):
     print 'main()'
@@ -9,12 +15,16 @@ def main(argv):
         argv = gflags.FLAGS(argv)
         print argv
     except gflags.FlagsError, e:
-        print '%s\\nUsage: %s ARGS\\n%s' % (e, argv[0], FLAGS)
+        print '%s\\nUsage: %s ARGS\\n%s' % (e, argv[0], gflags.FLAGS)
         sys.exit(1)
-    args = {
-        'granularity':'best',
-        }
-    locations = Latitude.location().list(**args).execute()
-    import pdb; pdb.set_trace()
+    some_date = date.today() - timedelta(days=1)
+    locations = Latitude().locations(some_date)
+    #.location().list(**args)
+    json = JSON(locations, some_date)
+    #kml = KML(locations)
+    FileSystem().write(json)
+
+    if gflags.FLAGS.debug:
+        import pdb; pdb.set_trace()
 
 main(sys.argv)
