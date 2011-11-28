4ca6ffad899938f53a1d7d225c77ffad51d371d3
David Schneider <david.schneider@picle.org>
2011-11-28 21:24:13 +0100

    add a filesystem export that puts the data in a year/month/day.format path on the filesystem

---
+++ latitude/exporter/filesystem.py
@@ -0,0 +1,23 @@
+import os
+from latitude.config import config
+
+class FileSystem(object):
+    def __init__(self):
+        pass
+
+    def write(self, data):
+        path = self.prepare_path(data)
+        filename = "%s%s.%s" % (path, data.date.day, data.extension)
+        with open(filename, 'w') as f:
+            f.write(str(data))
+
+    def prepare_path(self, data):
+        d = data.date
+        basepath = config.get('Filesystem', 'basepath')
+        basepath = os.path.expanduser(basepath)
+        sep = os.sep
+        path = "%s%s%d%s%d%s" % (basepath, sep, d.year, sep, d.month, sep)
+        if not os.path.isdir(path):
+            assert not os.path.exists(path)
+            os.makedirs(path)
+        return path
