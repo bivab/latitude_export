e546ec846682ef29e99617daeef36013189217fb
David Schneider <david.schneider@picle.org>
2011-12-03 16:22:55 +0100

    dropbox data exporter

---
+++ latitude/exporter/dropbox.py
@@ -0,0 +1,19 @@
+from latitude.exporter import Exporter
+from latitude.config import dropbox as db
+from oauth import oauth
+
+class Dropbox(Exporter):
+    def __init__(self):
+        self.client = db.get_client()
+
+    def write(self, data):
+        filename = self.build_filename(data)
+        self.client.put_file(filename, str(data), overwrite=True)
+
+    def build_filename(self, data):
+        d = data.date
+        sep = '/'
+        path = "%d%s%d%s%d.%s" % (d.year, sep,
+                                    d.month, sep,
+                                    d.day, data.extension)
+        return path
