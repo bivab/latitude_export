7c21e90badbf6da22e44cf0a48bfb0895cc1b6ab
David Schneider <david.schneider@picle.org>
2012-08-16 09:43:10 +0200

    use requirements txt to load the list of dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+requirements = open(join(dirname(__file__), 'requirements.txt')).readlines(),
+description = open(join(dirname(__file__), 'README.markdown')).read()
+import pdb; pdb.set_trace()
+
 setup(
     name='latitude',
     version='1.3',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.markdown')).read(),
+    long_description=description,
     entry_points={
         'console_scripts': ['latitude = latitude.__main__:entry_point']
     },
-    install_requires=['urllib3', 'apiclient',
-                'gdata', 'google-api-python-client',
-                'pytz', 'dropbox'],
-
+    install_requires=requirements,
     )
