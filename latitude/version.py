30ef633dd211ad48bc98dabca49affb8ef9b1e78
David Schneider <david.schneider@picle.org>
2012-12-03 19:15:44 +0100

    move version information to a module

---
+++ latitude/version.py
@@ -0,0 +1,5 @@
+__title__ = 'latitude'
+__version__ = '1.5'
+__author__ = 'David Schneider'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2012'
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
+from latitude import version
 from os.path import join, dirname
 
 requirements = open(join(dirname(__file__), 'requirements.txt')).readlines(),
 description = open(join(dirname(__file__), 'README.markdown')).read()
 
 setup(
-    name='latitude',
-    version='1.4',
+    name=version.__title__,
+    version=version.__version__,
     packages=find_packages(),
     long_description=description,
     entry_points={
